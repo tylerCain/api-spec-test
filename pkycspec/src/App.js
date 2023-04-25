d4a63aec4b8f30f5b14fc11a543e635fa15a6ae5
tylerCain <tyler.cain@kpmg.co.uk>
2023-04-25 14:56:45 +0100

    changes

--- pkycspec/src/App.js
+++ pkycspec/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import "swagger-ui-react/swagger-ui.css"
 import Home from "./routes/home"
 import APISpec from "./routes/apiSpec"
@@ -6,13 +6,13 @@ import HomeBar from "./navbar/navbar"
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <HomeBar />
       <Routes>
           <Route path="/" index element={<Home />} />
           <Route path="/spec1" element={<APISpec url="https://petstore3.swagger.io/api/v3/openapi.json" />} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
   /*return (
     <SwaggerUI url="https://petstore3.swagger.io/api/v3/openapi.json" />
