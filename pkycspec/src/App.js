40e1963797d0c0af329ba1edbde72cc74a665fe6
tylerCain <tyler.cain@kpmg.co.uk>
2023-04-25 15:42:03 +0100

    Revert

--- pkycspec/src/App.js
+++ pkycspec/src/App.js
@@ -4,17 +4,16 @@ import Home from "./routes/home"
 import APISpec from "./routes/apiSpec"
 import HomeBar from "./navbar/navbar"
 
+
 function App() {
   return (
-    <>
-      <HashRouter basename="/">
-        <HomeBar />
-        <Routes>
-            <Route path="/api-spec-test" index element={<Home />} />
-            <Route path="/spec1" element={<APISpec url="https://petstore3.swagger.io/api/v3/openapi.json" />} />
-        </Routes>
-      </HashRouter>
-    </>
+    <HashRouter>
+      <HomeBar />
+      <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="/spec1" element={<APISpec url="https://petstore3.swagger.io/api/v3/openapi.json" />} />
+      </Routes>
+    </HashRouter>
   )
   /*return (
     <SwaggerUI url="https://petstore3.swagger.io/api/v3/openapi.json" />
